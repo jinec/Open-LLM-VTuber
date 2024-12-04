368bfcaef9e7aed41b74e6c43fc4dcf58d61c6f2
jinec <mgitu125@126.com>
2024-12-04 06:15:48 +0000

    增加泛化词转换步骤

--- asr/fun_asr.py
+++ asr/fun_asr.py
@@ -43,6 +43,7 @@ class VoiceRecognition(ASRInterface):
 
         self.asr_with_vad = None
         self.hotwords = self.__load_hotwords()
+        self.generalization_dict = self.__load_GeneralizedWords()
 
     # Implemented in asr_interface.py
     # def transcribe_with_local_vad(self) -> str:
@@ -52,7 +53,16 @@ class VoiceRecognition(ASRInterface):
             lines = f.readlines()
             lines = [line.strip() for line in lines]
         return " ".join(lines)
-
+    
+    def __load_GeneralizedWords(self,):
+        generalizedWords_dictory = {}
+        with open("asr/generalizedWords.txt", "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            for line in lines:
+                line_words = line.strip().split(" ")
+                if len(line_words)==2:
+                    generalizedWords_dictory[line_words[0]] = line_words[1]
+        return generalizedWords_dictory
 
     def transcribe_np(self, audio: np.ndarray) -> str:
 
@@ -76,6 +86,24 @@ class VoiceRecognition(ASRInterface):
         full_text = re.sub(r"<\|.*?\|>", "", full_text)
         # the tags can also look like '< | en | > < | EMO _ UNKNOWN | > < | S pe ech | > < | wo itn | > ', so...
         full_text = re.sub(r"< \|.*?\| >", "", full_text)
+
+        # 泛化词典处理        
+        matches = []# 收集所有的匹配位置
+        for key in sorted(self.generalization_dict, key=len, reverse=True):  # 按长度降序排列
+            start = 0
+            while True:
+                start = full_text.find(key, start)
+                if start == -1:
+                    break
+                matches.append((start, start + len(key), key))
+                start += len(key)
+
+        # 对匹配项按结束位置排序，以便从后往前替换
+        matches.sort(key=lambda x: x[1], reverse=True)
+
+        # 执行替换
+        for start, end, key in matches:
+            full_text = full_text[:start] + self.generalization_dict[key] + full_text[end:]
 
         return full_text.strip()
 
